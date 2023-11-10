f80416cad1a0ae7447b1b54c59a6f0293f3b8c4f
Elysia Gao <39887684+EGpineapples@users.noreply.github.com>
2023-11-10 19:08:30 +0900

    Update infogan_cgan_integration.py

--- implementations/infogan/infogan_cgan_integration.py
+++ implementations/infogan/infogan_cgan_integration.py
@@ -58,16 +58,11 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        # Assuming self.init_size and input_dim for self.l1 are predefined or calculated
-        self.init_size = opt.img_size // 4
-        input_dim_for_l1 = 128 * self.init_size ** 2  # Example, adjust as per your model
+        self.label_emb = nn.Embedding(opt.n_classes, opt.n_classes)
 
-        # Calculate the label embedding dimension dynamically
-        label_embedding_dim = input_dim_for_l1 - (opt.latent_dim + opt.code_dim)
-
-        self.label_emb = nn.Embedding(opt.n_classes, label_embedding_dim)
-
-        self.l1 = nn.Sequential(nn.Linear(input_dim_for_l1, 128 * self.init_size ** 2))
+        input_dim = opt.latent_dim + opt.n_classes + opt.code_dim
+        self.init_size = opt.img_size // 4  # Initial size before upsampling
+        self.l1 = nn.Sequential(nn.Linear(input_dim, 128 * self.init_size ** 2))
 
         self.conv_blocks = nn.Sequential(
             nn.BatchNorm2d(128),
@@ -84,12 +79,17 @@ class Generator(nn.Module):
         )
 
     def forward(self, noise, labels, code):
-        labels_embed = self.label_emb(labels).view(labels.size(0), -1)
+        # Embed the labels
+        labels_embed = self.label_emb(labels)
+    
+        # Concatenate label embedding, noise, and code
         gen_input = torch.cat((noise, labels_embed, code), -1)
+    
         out = self.l1(gen_input)
         out = out.view(out.shape[0], 128, self.init_size, self.init_size)
         img = self.conv_blocks(out)
         return img
+
 
 class Discriminator(nn.Module):
     def __init__(self):
