0009305af975a1cd9528f79d3ec650fd152466ff
Elysia Gao <39887684+EGpineapples@users.noreply.github.com>
2023-11-11 01:07:07 +0900

    Update infogan_working.py

--- implementations/infogan/infogan_working.py
+++ implementations/infogan/infogan_working.py
@@ -180,7 +180,7 @@ mnist_dataset = datasets.MNIST(
 
 # Transformations
 transform = transforms.Compose([
-    transforms.Resize(opt['img_size']),
+    transforms.Resize(opt.img_size),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalizing for RGB channels
 ])
